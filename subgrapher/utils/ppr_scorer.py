36f1262ee4cd5f83354b5d3667419563e9fd1c88
thassung <105700459+thassung@users.noreply.github.com>
2025-06-05 03:49:03 +0700

    add comment to ppr_scorer uses

--- subgrapher/utils/ppr_scorer.py
+++ subgrapher/utils/ppr_scorer.py
@@ -15,6 +15,8 @@ def personalized_pagerank(G, seeds, alpha=0.85, weight=None):
 def extract_topk_subgraph_pyg(data, u, v, k=100, alpha=0.5, ppr_alpha=0.85):
     """
     Extracts top-k subgraph from data, with u and v as seeds.
+    data: PyG Data object (can be created using load_txt_to_pyg)
+    u, v: node indices (as used in data)
     Returns: PyG Data object
     """
     G = to_networkx(data, to_undirected=True)
@@ -47,5 +49,13 @@ def extract_topk_subgraph_pyg(data, u, v, k=100, alpha=0.5, ppr_alpha=0.85):
         old_to_new = {old: i for i, old in enumerate(subG.nodes())}
         sub_data.x = data.x[torch.tensor(list(subG.nodes()))]
     return sub_data
+
+# from loader import load_txt_to_pyg
+# from ppr_scorer import extract_topk_subgraph_pyg
+# data_path = 'data/FB15K237/test.txt'
+# data, node2idx, idx2node = load_txt_to_pyg(data_path)
+# u = node2idx['/m/08966']
+# v = node2idx['/m/05lf_']
+# sub_data = extract_topk_subgraph_pyg(data, u, v, k=100)
 
 
